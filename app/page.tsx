a8790b662becce810456bcbb4d377ad3417f7d2f
tahsin <amin1996bangladesh@gmail.com>
2025-12-06 22:53:54 +0600

    feat: Add Hero section (headline + subtext)

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,14 @@
 export default function Home() {
   return (
-    <div className="flex-1 bg-zinc-50 font-sans dark:bg-black">
-      <div className="div1 bottom-left h-screen pt-20">
-        <h1>Home</h1>
-        <p>We learn from every experience by pushing the boundaries past the ordinary. Let's create best website experience, together.</p>
-        <button>Book a call</button>
-      </div>
+    <div className="section-hero flex-1 bg-zinc-50 font-sans dark:bg-black container">
+        <h1>Tech
+          <br/>
+          for humans?
+        </h1>
+        <div>
+          <h1 className="text-end">We make it happen</h1>
+          <p>We learn from every experience by pushing the boundaries past the ordinary. Let's create best website experience, together.</p>
+        </div>
     </div>
   );
 }
