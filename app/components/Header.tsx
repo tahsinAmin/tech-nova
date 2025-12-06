34cb77dfd0687b2771151f0646349d8aa8cf8b48
tahsin <amin1996bangladesh@gmail.com>
2025-12-06 22:49:33 +0600

    feat: Add header and Navbar Menu

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,8 +1,19 @@
-const Header = () => {
+"use client";
+import Link from 'next/link';
+import React from 'react'
 
+const Header = () => {
+  const [showMenu, setShowMenu] = React.useState(false);
   return (
-    <div className=''>
-      Header
+    <div className='container'>
+      <nav className='navbar flex justify-between'>
+        <Link href="/" onClick={() => setShowMenu(false)}>Logo</Link>
+        <div className='hamburger' onClick={() => setShowMenu(!showMenu)}>=</div>
+      </nav>
+      <div className={`navbar-main-wrapper ${showMenu ? 'show' : ''}`}>
+      <Link href="/about" onClick={() => setShowMenu(false)} className='navlink-text-2'>About</Link>
+      <Link href="/contact" onClick={() => setShowMenu(false)} className='navlink-text-2'>Contact</Link>
+      </div>
     </div>
   )
 }
