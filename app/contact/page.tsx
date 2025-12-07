ffab0887dcaeff3a41883b1e5ec3c15bc0e8a776
tahsin <amin1996bangladesh@gmail.com>
2025-12-07 11:41:27 +0600

    feat: [Contact Page] - Basic validation (e.g., required fields)

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -3,37 +3,41 @@ export default function Contact() {
     <>
       <div className="section-hero layout-contact flex-1 bg-zinc-50 font-sans dark:bg-black container">
         <h1>Need some assistance or curious about who we are?</h1>
-        <small>Send us a messsage</small>
+        <div className="small-text">Send us a messsage</div>
         <form action="/contact" className="flex flex-col gap-4 max-w-md">
-          <input 
-            type="text" 
-            placeholder="So, what's your name?" 
+          <input
+            type="text"
+            required
+            placeholder="So, what's your name?"
             className="w-full px-2 py-2 bg-transparent border-0 border-b border-gray-800 focus:outline-none focus:ring-0 font-medium placeholder:font-medium placeholder:text-black"
           />
-          <input 
-            type="email" 
-            placeholder="What's your email?" 
+          <input
+            required
+            type="email"
+            placeholder="What's your email?"
             className="w-full px-2 py-2 bg-transparent border-0 border-b border-gray-800 focus:outline-none focus:ring-0 font-medium placeholder:font-medium placeholder:text-black"
           />
-          <input 
-            type="text" 
-            placeholder="What's your message?" 
+          <input
+            required
+            type="text"
+            placeholder="What's your message?"
             className="w-full px-2 py-2 bg-transparent border-0 border-b border-gray-800 focus:outline-none focus:ring-0 font-medium placeholder:font-medium placeholder:text-black"
           />
-          <button 
-            type="submit" 
+          <button
+            type="submit"
             className="cta"
           >
             Send
           </button>
-          <div className="text-center">or</div>
-          <button 
-            type="submit" 
-            className="cta"
-          >
-            Book a call
-          </button>
+
         </form>
+        <div className="text-center py-[16px]">or</div>
+        <button
+          type="submit"
+          className="cta"
+        >
+          Book a call
+        </button>
       </div>
     </>
   );
