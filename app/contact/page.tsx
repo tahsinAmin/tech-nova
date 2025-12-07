87f52f105e09c8f5ea44c152d63b50d4207f3c74
tahsin <amin1996bangladesh@gmail.com>
2025-12-07 13:15:38 +0600

    feat: [Contact Page] - Success state after submit

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,43 +1,71 @@
+'use client';
+
+import { useState } from "react";
+
 export default function Contact() {
+  const [formMsg, setFormMsg] = useState("");
+  const [loading, setLoading] = useState(false);
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    setLoading(true);
+    setTimeout(() => {
+      setFormMsg("Thanks for your message!");
+      setLoading(false);
+    }, 2000);
+  }
+
   return (
     <>
       <div className="section-hero layout-contact flex-1 bg-zinc-50 font-sans dark:bg-black container">
-        <h1>Need some assistance or curious about who we are?</h1>
-        <div className="small-text">Send us a messsage</div>
-        <form action="/contact" className="flex flex-col gap-4 max-w-md">
-          <input
-            type="text"
-            required
-            placeholder="So, what's your name?"
-            className="w-full px-2 py-2 bg-transparent border-0 border-b border-gray-800 focus:outline-none focus:ring-0 font-medium placeholder:font-medium placeholder:text-black"
-          />
-          <input
-            required
-            type="email"
-            placeholder="What's your email?"
-            className="w-full px-2 py-2 bg-transparent border-0 border-b border-gray-800 focus:outline-none focus:ring-0 font-medium placeholder:font-medium placeholder:text-black"
-          />
-          <input
-            required
-            type="text"
-            placeholder="What's your message?"
-            className="w-full px-2 py-2 bg-transparent border-0 border-b border-gray-800 focus:outline-none focus:ring-0 font-medium placeholder:font-medium placeholder:text-black"
-          />
-          <button
-            type="submit"
-            className="cta"
-          >
-            Send
-          </button>
+        <h1 className="sm:mb-[94px] text-[50px] font-semibold mb-[10px]">Need some assistance or curious about who we are?</h1>
+        <div className="px-[24px]">
+          {formMsg ? null : <div className="small-text sm:mt-0">Send us a messsage</div>}
+          <div className="sm:flex sm:gap-4 sm:justify-between">
+            {formMsg ?
+              <div className="text-center mt-[48px] mb-[15px]">Thanks for your message!</div> :
+              (<form onSubmit={handleSubmit} className="flex flex-col gap-4 max-w-md sm:w-4/5 ">
+                <input
+                  type="text"
+                  required
+                  name='username'
+                  placeholder="So, what's your name?"
+                  className="w-full px-2 py-2 bg-transparent border-0 border-b border-gray-800 focus:outline-none focus:ring-0 font-medium placeholder:font-medium placeholder:text-black"
+                />
+                <input
+                  required
+                  type="email"
+                  name='email'
+                  placeholder="What's your email?"
+                  className="w-full px-2 py-2 bg-transparent border-0 border-b border-gray-800 focus:outline-none focus:ring-0 font-medium placeholder:font-medium placeholder:text-black"
+                />
+                <input
+                  required
+                  type="text"
+                  name='message'
+                  placeholder="What's your message?"
+                  className="w-full px-2 py-2 bg-transparent border-0 border-b border-gray-800 focus:outline-none focus:ring-0 font-medium placeholder:font-medium placeholder:text-black"
+                />
+                <button
+                  type="submit"
+                  className="cta sm:w-[110px]"
+                >
+                  {loading ? "Sending..." : "Send"}
+                </button>
 
-        </form>
-        <div className="text-center py-[16px]">or</div>
-        <button
-          type="submit"
-          className="cta"
-        >
-          Book a call
-        </button>
+              </form>)}
+
+            <div className="flex flex-col sm:w-1/5 sm:flex-row sm:justify-between sm:items-start">
+              <div className="text-center py-[16px]">or</div>
+              <button
+                type="submit"
+                className="cta"
+              >
+                Book a call
+              </button>
+            </div>
+          </div>
+        </div>
       </div>
     </>
   );
