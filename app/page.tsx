5292c38994875272383d1ed0eae82b391b5dcc68
tahsin <amin1996bangladesh@gmail.com>
2025-12-09 01:23:20 +0600

    feat: [Homepage Page] - Services Section

--- app/page.tsx
+++ app/page.tsx
@@ -7,15 +7,31 @@ export default function Home() {
           to launch?
         </h1>
         <div className="hero-bottom sm:flex sm:flex-row-reverse sm:items-end gap-4 justify-between">
-          <h1 className="text-end sm:w-2/5">We make
+          <h1 className="text-end">We make
             <br />
             it live.
           </h1>
           <p className="description">We learn from every experience by pushing the boundaries past the ordinary. Let's create best website experience, together.</p>
         </div>
       </div>
-      <div className="services">
-        <div className="what-we-do">
+      <div className="services pageWidth">
+        <h2 className="text-center text-2xl font-semibold pb-4">Our Services</h2>
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-2">
+          <div className="productsBoxItem">
+            <div className="productsBoxItemSmall">
+              <h4>Voice</h4>
+            </div>
+          </div>
+          <div className="productsBoxItem">
+            <div className="productsBoxItemSmall">
+              <h4>Messaging</h4>
+            </div>
+          </div>
+          <div className="productsBoxItem">
+            <div className="productsBoxItemSmall">
+              <h4>Platform</h4>
+            </div>
+          </div>
         </div>
       </div>
     </>
