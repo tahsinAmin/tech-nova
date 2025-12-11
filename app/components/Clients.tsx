bb47c0f612ed98d2fdf6a2a14030c43ebafc43a6
tahsin <amin1996bangladesh@gmail.com>
2025-12-12 02:23:54 +0600

    feat: [Homepage Page] - Testimonials Section

---
+++ app/components/Clients.tsx
@@ -0,0 +1,33 @@
+
+
+const Clients = () => {
+    return (
+        <div className="clients container mx-auto">
+
+            <ul id="cards">
+                <li className="card" id="card1">
+                    <div className="card-body">
+                        <h2>Clients</h2>
+                    </div>
+                </li>
+                <li className="card" id="card2">
+                    <div className="card-body">
+                        <h2>Card 2</h2>
+                    </div>
+                </li>
+                <li className="card" id="card3">
+                    <div className="card-body">
+                        <h2>Card 3</h2>
+                    </div>
+                </li>
+                <li className="card" id="card4">
+                    <div className="card-body">
+                        <h2>Card 4</h2>
+                    </div>
+                </li>
+            </ul>
+        </div>
+    )
+}
+
+export default Clients
